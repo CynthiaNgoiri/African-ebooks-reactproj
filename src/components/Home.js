17e94112f6cb4d581a54612c15733749f69e6cd0
Cynthia Njomo <cynthiangoirinjomo@gmail.com>
2022-10-06 21:34:18 +0300

    created home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,1 @@
-import React from "react";
-import Book from "./Book";
 
-function Home({ ebooks }) {
-  return (
-    <div>
-      {ebooks.map((ebook) => {
-        return (
-        <Book key={ebook.id} ebook ={ebook}/>
-        );
-      })}
-    </div>
-    
-  );
-}
-
-export default Home;
