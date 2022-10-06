a56e787f0e5d64a62cb2a1d8deca3c4a7d19c989
Cynthia Njomo <cynthiangoirinjomo@gmail.com>
2022-10-06 21:42:03 +0300

    successful filter

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,31 +1,33 @@
-import React, {useState, useEffect} from 'react' 
-import Home from './Home'
-import Search from './Search'
-
+import React, { useState, useEffect } from "react";
+import Home from "./Home";
+import Search from "./Search";
 
 function Main() {
-  const [ebooks, setEbooks] = useState([])
-  const [searchValue,setSearchValue] = useState([])
- 
-  useEffect(() =>{
+  const [ebooks, setEbooks] = useState([]);
+  const [searchValue, setSearchValue] = useState([]);
+
+  useEffect(() => {
     fetch("http://localhost:3002/ebooks")
-    .then((res) => res.json())
-    .then((resp)=>{
-      setEbooks(resp)
-      setSearchValue(resp)
-    })
-  },[])
- 
-  function change(event){
-    setEbooks(searchValue.filter(ebooks=>ebooks.title.toLowerCase().includes(event.target.value.toLowerCase())))
-   }
+      .then((res) => res.json())
+      .then((resp) => {
+        setEbooks(resp);
+        setSearchValue(resp);
+      });
+  }, []);
+
+  function change(event) {
+    setEbooks(
+      searchValue.filter((ebooks) =>
+        ebooks.title.toLowerCase().includes(event.target.value.toLowerCase())
+      )
+    );
+  }
   return (
     <div>
-      <Search change={change}/>
-      <Home ebooks = {ebooks}/>
-
+      <Search change={change} />
+      <Home ebooks={ebooks} />
     </div>
-  )
+  );
 }
 
 export default Main;
